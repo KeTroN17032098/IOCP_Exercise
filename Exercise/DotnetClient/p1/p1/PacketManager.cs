0ee22e7cfc1411f931a6c18ae09cbc5e6f412a39
KeTroN17032098 <64875428+KeTroN17032098@users.noreply.github.com>
2022-07-13 16:40:23 +0900

    ads

--- Exercise/DotnetClient/p1/p1/PacketManager.cs
+++ Exercise/DotnetClient/p1/p1/PacketManager.cs
@@ -17,71 +17,23 @@ namespace p1
         Y = 1,
         N
     };
-    enum  LOBBY_PROTOCOL
-    {
-        REQ_LOBBYDATA=29,
-		LOBBYDATA
-    };
-    enum  LOBBYDATA_PROTOCOL
-    {
-        PROFILE=54,
 
-	};
-    enum  LOBBYMANAGER_DETAIL
-    {
-        MSG=1,
-	};
-	enum  LORS
-	{
-		SELCTION=76,
-		LOGIN,
-		SIGNIN
-	};
-
-	enum  SELECTION
-	{
-		REQ_SELECTIONMSG=32,
-		SELECTION_MSG,
-		SELECTION_SET
-	};
-
-	enum  LOGIN_PROTOCOL
-	{
-		LOGIN_MSG = 25,
-		LOGIN_TRY,
-		LOGIN_SUCCESS,
-		LOGIN_ERROR
-	};
-
-	enum  SIGNIN_PROTOCOL
-	{
-		SIGNIN_MSG = 42,
-		SIGNIN_TRY,
-		SIGNIN_SUCCESS,
-		SIGNIN_ERROR
-	};
-
-	enum  LOGINMANAGER_ERR
-	{
-		MULTIPLEID = -472,// 회원가입 아이디중복
-		LENGTHLIMIT,// 최대 길이 초과
-		SMALLCHAR,//공백란 있음
-		ALREADYLGIN,//이미 로그인 됨
-		NOMATCH,//아이디 혹은 비밀번호 오류
-		UNKNOWNERR,//알수없는 오류
-		NOERR//오류 없음
-	};
-
-	enum  LOGINMANAGER_DETAIL
-	{
-		NUMBER=1,
-		MSG=1<<1,
-		ERRCODE=1<<2,
-		IDPW=1<<3,
-		PUBLICKEY=1<<4
-	};
 	class PacketManager
     {
-		
+        private static PacketManager instance;
+        public static void CreateInstance()
+        {
+            if (instance == null) instance = new PacketManager();
+        }
+        public static void ClearInstance()
+        {
+            if (instance != null) instance = null;
+        }
+        public static PacketManager GetInstance => instance;
+
+        private PacketManager()
+        { }
+
+        public 
     }
 }
